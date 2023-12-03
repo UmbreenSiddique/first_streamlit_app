85f117353e2ea4fd60bffc7e76bfbb059bb4450d
UmbreenSiddique <152490501+UmbreenSiddique@users.noreply.github.com>
2023-12-03 14:28:08 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
+streamlit.body('Omega 3 & Blueberry Oatmeal')
+streamlit.body('Kale, Spinach & Rocket Smoothie')
+streamlit.body('Hard-Boiled Free-Range Egg')
